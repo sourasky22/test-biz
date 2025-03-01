f4f1e76440f69688c115fe18aed1893db6342e38
sourasky22 <sourasky22@gmail.com>
2025-03-02 01:28:19 +0200

    reset config files

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,14 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // Ensure this is set correctly
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
+
 
